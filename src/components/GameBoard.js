5ff281e24930376fb1eac9a20f60dc806822da02
lalaro <100946295+lalaro@users.noreply.github.com>
2024-10-25 18:17:27 -0500

    Cambios de versión anterior hasta donde funciona

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import baseImage from '../assets/images/map/base.png';
+import treeImage from '../assets/images/map/trees.png';
 import wallBrickImage from '../assets/images/map/wall_brick.png';
 import wallSteelImage from '../assets/images/map/wall_stell.png';
-import treeImage from '../assets/images/map/trees.png';
-import baseImage from '../assets/images/map/base.png';
-import playerImage from '../assets/images/tank.png';
 import '../styles/GameBoard.css';
 import PlayerController from './PlayerController';
-
+ 
 // El mapa del juego representado en una matriz 2D
 // 0 = vacío, 1 = muro de ladrillo, 2 = muro de acero, 3 = árboles, 4 = base
 const mapData = [
@@ -36,8 +35,8 @@ const mapData = [
   [0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 2, 2, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]
 ];
-
-
+ 
+ 
 const getTileImage = (tile) => {
   switch (tile) {
     case 1:
@@ -53,14 +52,14 @@ const getTileImage = (tile) => {
       return null;
   }
 };
-
+ 
 const GameBoard = () => {
-  /*const [playerPosition, setPlayerPosition] = useState({ x: 2, y: 2 });*/
-/*
+  /*const [playerPosition, setPlayerPosition] = useState({ x: 2, y: 2 });
+ 
   const handleKeyDown = (event) => {
     const { key } = event;
     let newPosition = { ...playerPosition };
-
+ 
     switch (key) {
       case 'w':
         newPosition.y = Math.max(0, playerPosition.y - 1);
@@ -81,7 +80,7 @@ const GameBoard = () => {
       setPlayerPosition(newPosition);
     }
   };
-
+ 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
     return () => {
@@ -100,21 +99,24 @@ const GameBoard = () => {
             const tileImage = getTileImage(tile);
             return (
               <div className="game-board__tile" key={colIndex}>
+                {/*tile*/}
                 {tileImage && <img src={tileImage} alt="tile" />}
+ 
                 {/* Renderizar el jugador en la posición actual */}
-                {//rowIndex === playerPosition.y && colIndex === playerPosition.x && ( 
+                {//rowIndex === playerPosition.y && colIndex === playerPosition.x && (
                   //<img src={playerImage} alt="player" className="player" />
                 //)
-                } 
+                }
               </div>
             );
           })}
         </div>
       ))}
       {/* Render PlayerController */}
-      <PlayerController />
+      <PlayerController mapData={mapData}/>
     </div>
+       
   );
 };
-
+ 
 export default GameBoard;
