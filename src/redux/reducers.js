89b8013d951f795e7d56f2ef5b1ed53a5ddb7f0f
lalaro <100946295+lalaro@users.noreply.github.com>
2024-12-06 00:47:59 -0500

    Replace this switch reducers CHECK

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,20 +1,18 @@
 const initialState = {
     game: {
         players: [],
-        // otros elementos del estado
     },
 };
 
 const gameReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'UPDATE_GAME_STATE':
-            return {
-                ...state,
-                game: action.payload,
-            };
-        default:
-            return state;
+    if (action.type === 'UPDATE_GAME_STATE') {
+        return {
+            ...state,
+            game: action.payload,
+        };
     }
+s
+    return state;
 };
 
 export default gameReducer;
